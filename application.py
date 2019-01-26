16d3b3f74bbfd1cbe8fcceeda4b0ebf986e0c66c
neelb99 <neelbajaj1234@hotmail.com>
2019-01-26 19:42:25 +0530

    functionality without db

--- application.py
+++ application.py
@@ -9,3 +9,22 @@ def index():
 @app.route("/login")
 def login():
 	return render_template('login.html')
+
+@app.route("/admin", methods = ["POST"])
+def admin():
+	username = request.form.get("username")
+	password = request.form.get("password")
+	check = username=="admin" and password=="admin"
+	return render_template('admin.html', check=check)
+
+
+@app.route("/update", methods = ["POST"])
+def update():
+	ttval = request.form.get("tt")
+	psval = request.form.get("ps4")
+	return render_template("index.html",ttval=ttval, psval=psval)
+
+
+
+
+
